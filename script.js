352bc95ffc425ecd7eefadba89a33a1fbd8aadb8
achowdhry7 <a.chowdhry.8388@westcliff.edu>
2026-01-03 21:31:31 -0800

    All changes

--- script.js
+++ script.js
@@ -136,6 +136,18 @@ document.querySelectorAll('.group-title').forEach(btn => {
     });
 });
 
+const reveals = document.querySelectorAll(".reveal");
 
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    { threshold: 0.25 }
+);
 
-
+reveals.forEach(el => observer.observe(el));
